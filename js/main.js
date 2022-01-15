182a6cb8a4fc138866cbd3ae14814ab049b2b5c3
verenasworld <verena.felizidas@gmail.com>
2022-01-15 11:20:55 +0100

    formating code

--- js/main.js
+++ js/main.js
@@ -1,58 +1,50 @@
 const draggableElements = document.querySelectorAll(".draggable");
 const droppableElements = document.querySelectorAll(".droppable");
 
-draggableElements.forEach(elem =>{
+draggableElements.forEach(elem => {
   elem.addEventListener("dragstart", dragStart);
-   // elem.addEventListener("drag", drag);
-   // elem.addEventListener("dragend", dragEnd);
+  // elem.addEventListener("drag", drag);
+  // elem.addEventListener("dragend", dragEnd);
 })
-droppableElements.forEach(elem =>{
+droppableElements.forEach(elem => {
   elem.addEventListener("dragenter", dragEnter);
   elem.addEventListener("dragover", dragOver);
   elem.addEventListener("dragleave", dragLeave);
-   elem.addEventListener("drop", drop);
+  elem.addEventListener("drop", drop);
 })
 //drag and drop Function
 
-function dragStart(event){
-  console.log("dragging......");
+function dragStart(event) {
   event.dataTransfer.setData("text", event.target.id);
-
 }
 
-function dragEnter(event){
-  if(!event.target.classList.contains("dropped")){
+function dragEnter(event) {
+  if (!event.target.classList.contains("dropped")) {
     event.target.classList.add("droppable-hover")
   }
-
-
 }
 
-function dragOver(event){
-  if(!event.target.classList.contains("dropped")){
+function dragOver(event) {
+  if (!event.target.classList.contains("dropped")) {
     event.preventDefault();
   }
-
 }
 
-function dragLeave(event){
+function dragLeave(event) {
 
-  if(!event.target.classList.contains("dropped")){
+  if (!event.target.classList.contains("dropped")) {
     event.target.classList.remove("droppable-hover")
-
   }
-
 }
 
-
-function drop(event){
+function drop(event) {
   event.preventDefault();
   let draggableElementData;
   let droppableElementData;
   draggableElementData = event.dataTransfer.getData("text");
   droppableElementData = event.target.getAttribute("data-draggable-id")
 
-  if(draggableElementData === droppableElementData){
+  if (draggableElementData === droppableElementData) {
     event.target.classList.add("dropped");
     let draggableElement = document.getElementById(draggableElementData);
     event.target.style.backgroundColor = draggableElement.style.color;
